7b7dc504bd746970a4054168c4db3bfda8cd099c
drybashlykov <drybashlykov@gmail.com>
2019-08-14 19:28:31 +0300

    same data for all markers bug fixed

--- ensembleAverage.m
+++ ensembleAverage.m
@@ -4,17 +4,36 @@ function [ output_table ]  = ensembleAverage(handles, pacingCL_t, truncateAfter_
     truncateAfter = truncateAfter_t*Fs/1000;
     startingFrame = 1 + startingTime_t*Fs/1000;
     
+    offset_t = 10;
+    threshold = 0.5;
+    offset = offset_t*Fs/1000;
+    
     output_length = min(min(pacingCL, truncateAfter + 1), handles.maxFrame);
+    
+    markers = handles.activeCamData.markers;
+    
+    AP = squeeze(handles.activeCamData.cmosData(markers(1,2), markers(1,1), :));
+        if startingTime_t == -1
+            index = find(AP(1:(pacingCL*2)) < threshold);
+            if size(index, 1) > 2
+                spaces = index(2: end) - index(1: end - 1);
+                [~,startingFrame] = max(spaces);
+                if startingFrame - offset > 0
+                    startingFrame = startingFrame - offset;
+                end
+            end
+        end
     
     APcount = floor((handles.maxFrame - startingFrame + pacingCL - output_length) / pacingCL);
     
-    markers = handles.activeCamData.markers;
+    
     [markersCount,~] = size(markers);
     output = zeros(output_length, 1 + markersCount*(APcount + 1));
     output(:,1) = handles.time(1:output_length)';
     APs = zeros(output_length, APcount);
     variableNames = "t";
-    for i = 1:markersCount
+    
+    for i = 1:markersCount 
         AP = squeeze(handles.activeCamData.cmosData(markers(i,2), markers(i,1), :));
         for j = 1:APcount
             variableNames = variableNames + " m" + num2str(i) + "ap" + num2str(j) + " ";
